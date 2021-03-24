9a572bafea1c64213e199e87c269613cbf807790
Tristan Tarpley <tristan@learnwithjuni.com>
2021-03-23 22:14:59 -0500

    set up dirty POC for data dump experience

---
+++ src/app/dataset/toolbar/NewRows/FileUpload.tsx
@@ -0,0 +1,6 @@
+import DatasetUploaderToSocket from 'components/DatasetUploader/DatasetUploaderToSocket';
+import React, { FC } from 'react';
+
+const FileUpload: FC = () => <DatasetUploaderToSocket />;
+
+export default FileUpload;
