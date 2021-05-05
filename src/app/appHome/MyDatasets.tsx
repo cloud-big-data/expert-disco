9cf4bbfc6b4041a719aff65a081689bfebbe60f1
Tristan Tarpley <tristan@learnwithjuni.com>
2021-05-05 10:13:45 -0500

    did some stuff

--- src/app/appHome/MyDatasets.tsx
+++ src/app/appHome/MyDatasets.tsx
@@ -15,7 +15,7 @@ const MyDatasetsContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
-  height: 100%;
+  min-height: 100%;
   .empty__container {
     display: flex;
     flex-direction: column;
