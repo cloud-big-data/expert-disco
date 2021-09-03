d4e487e8b054e874c9d10591d934b8ec074d2e23
Tristan Tarpley <me@tristantarpley.com>
2021-09-03 16:41:48 -0500

    remove numExports functionality

--- src/app/dataset/dataset_aggregates/DatasetExport/DatasetExport.tsx
+++ src/app/dataset/dataset_aggregates/DatasetExport/DatasetExport.tsx
@@ -4,10 +4,8 @@ import { Steps } from 'antd';
 import { Destinations } from 'app/dataset/types';
 import DatasetContext from 'contexts/DatasetContext';
 import { Text } from 'components/ui/Typography';
-import InputField from 'components/ui/InputField';
 import { ButtonPrimary } from 'components/ui/Buttons';
 import SingleSelect from 'components/SingleSelect';
-import Separator from 'components/Separator';
 import SkyvueExport from './Destinations/SkyvueExport';
 
 const { Step } = Steps;
@@ -37,8 +35,6 @@ const DatasetExport: React.FC = () => {
     undefined,
   );
 
-  const [numExports, setNumExports] = useState(1);
-
   return (
     <ExportContainer>
       <div className="steps__container">
@@ -47,11 +43,6 @@ const DatasetExport: React.FC = () => {
             title="Pick a destination"
             description="Where would you like us to send this dataset?"
             onClick={() => setStep(0)}
-          />
-          <Step
-            title="How should we send it?"
-            description="Just a few more details"
-            onClick={() => (destination ? setStep(1) : undefined)}
           />
           <Step title="Access your data" />
         </Steps>
@@ -69,63 +60,39 @@ const DatasetExport: React.FC = () => {
               selected={destination}
             />
             <div className="buttons">
-              <ButtonPrimary onClick={() => setStep(1)} disabled={!destination}>
+              <ButtonPrimary
+                onClick={() => {
+                  setStep(1);
+                  socket?.emit('exportToCsv', {
+                    title: datasetHead?.title,
+                    destination,
+                  });
+                }}
+                disabled={!destination}
+              >
                 Continue
               </ButtonPrimary>
             </div>
           </div>
         )}
         {step === 1 &&
-          (destination === 'csv' ? (
-            <div className="output-settings__container">
-              <h5>How many files should we export?</h5>
-              <Text size="sm" len="short">
-                Different apps require different sized files. Skyvue allows you to
-                export your dataset to as many files as you need in order to get
-                manageable file sizes that you can open in all of your favorite data
-                tools.
-              </Text>
-              <InputField
-                value={numExports}
-                onChange={e => {
-                  if (parseInt(e.target.value, 10) > 0 || e.target.value === '') {
-                    setNumExports(parseInt(e.target.value, 10));
-                  }
-                }}
-                type="number"
-                prepend="files"
-              />
-              <Separator />
-              <ButtonPrimary
-                onClick={() => {
-                  socket?.emit('exportToCsv', {
-                    title: datasetHead.title,
-                    quantity: numExports ?? 1,
-                  });
-                  setStep(2);
-                }}
-                disabled={Number.isNaN(numExports) || numExports < 1}
-              >
-                Start download
-              </ButtonPrimary>
-            </div>
-          ) : destination === 'skyvue' ? (
+          (destination === 'skyvue' ? (
             <div className="output-settings__container">
               <SkyvueExport />
             </div>
+          ) : destination === 'csv' ? (
+            <div className="output-settings__container">
+              <h5>Your download has started</h5>
+              <Text size="sm" len="short">
+                It may take a few moments for the dataset to be fully downloaded,
+                depending on the size of the file.
+              </Text>
+            </div>
           ) : (
-            ''
+            <div className="output-settings__container">
+              <p>Sorry, we don't yet support {destination} as a destination!</p>
+            </div>
           ))}
-
-        {step === 2 && (
-          <div className="output-settings__container">
-            <h5>Your download has started</h5>
-            <Text size="sm" len="short">
-              It may take a few moments for the dataset to be fully downloaded,
-              depending on the size of the file.
-            </Text>
-          </div>
-        )}
       </div>
     </ExportContainer>
   );
